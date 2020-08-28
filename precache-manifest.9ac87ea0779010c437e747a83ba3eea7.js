a6138586f04fb1e00ae66d08f50a616ca6abe2db
Ian <41586905+ianbaxter@users.noreply.github.com>
2020-08-28 02:50:52 +0100

    Updates

---
+++ precache-manifest.9ac87ea0779010c437e747a83ba3eea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf2d6b4b8c74cae5cf9c8255ccc70af",
+    "url": "/Collager/index.html"
+  },
+  {
+    "revision": "f2735825e207b58ebad5",
+    "url": "/Collager/static/css/main.51912219.chunk.css"
+  },
+  {
+    "revision": "82a79d13a0831bbc7c89",
+    "url": "/Collager/static/js/2.135a79b4.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/Collager/static/js/2.135a79b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2735825e207b58ebad5",
+    "url": "/Collager/static/js/main.ecba9f26.chunk.js"
+  },
+  {
+    "revision": "456442d39614560d27d2",
+    "url": "/Collager/static/js/runtime-main.45e0f117.js"
+  }
+]);
